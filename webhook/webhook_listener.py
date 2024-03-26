12452cb718668c6b43dd487d1909e279fa73a985
Erik <erik.boinski@posteo.de>
2024-03-26 13:01:17 +0100

    Added autoupdater via webhook_listener.py and update.sh, added Club-Mate to responses.py

---
+++ webhook/webhook_listener.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import subprocess
+import logging
+
+logging.basicConfig(filename='webhook.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def handle_webhook():
+    data = request.json
+    logging.info("Webhook received %s", data)
+    if data['event'] == 'push':
+        subprocess.call(['./update.sh'])
+        return jsonify({'status': 'success'})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=3000)
