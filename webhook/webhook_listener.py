51e2c2e86df32b62ab4bf493fcde0b62262ca5ce
Mainsys <c.mieske@B124A-7009>
2024-03-27 10:32:46 +0100

    test

--- webhook/webhook_listener.py
+++ webhook/webhook_listener.py
@@ -25,15 +25,34 @@ if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3001)
 """
 
-from flask import Flask
+from flask import Flask, request, jsonify
+import subprocess
+import logging
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
+# Konfiguriere das Logging
+logging.basicConfig(level=logging.INFO, filename='git_webhooks.log', format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/webhook', methods=['POST'])
+def git_webhook():
+    # Verarbeite die Webhook-Daten
+    data = request.json
+    logging.info('Webhook received with data: %s', data)
+
+    # Versuche, ein 'git pull' Kommando auszuführen
+    try:
+        # Definiere den Pfad zum Repository
+        repo_path = '/home/ubuntu/pythonDiscordBot'
+        # Führe 'git pull' aus
+        subprocess.check_output(['git', '-C', repo_path, 'pull'])
+        logging.info('Git pull erfolgreich ausgeführt.')
+        return jsonify({'status': 'Git pull erfolgreich.'}), 200
+    except subprocess.CalledProcessError as e:
+        logging.error('Fehler beim Git pull: %s', e.output)
+        return jsonify({'status': 'Fehler beim Git pull.'}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=3000)
