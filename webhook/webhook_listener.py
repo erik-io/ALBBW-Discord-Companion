7fc7b79c1c11b6007ab218af6683b5bd2f6756fb
Mainsys <68498888+mainsys-git@users.noreply.github.com>
2024-03-27 09:54:17 +0100

    test

--- webhook/webhook_listener.py
+++ webhook/webhook_listener.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+"""from flask import Flask, request, jsonify
 import subprocess
 import logging
 
@@ -23,3 +23,17 @@ def handle_webhook():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3001)
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
