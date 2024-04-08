f2b7a797203cfad849f2b11f92365fd63c0ede55
Erik Boinski <erik.boinski@posteo.de>
2024-04-09 00:29:02 +0200

    :ambulance: Critical hotfix.

--- main.py
+++ main.py
@@ -219,8 +219,6 @@ async def check_new_mails():
                 else:
                     await channel.send(f"In KW {current_kw + i} gibt es keine veganen Mahlzeiten.")
 
-bot.add_command(weather)
-
 def main():
     """
     The main function of the bot. It checks for new emails and then runs the bot.
@@ -251,6 +249,8 @@ try:
         raise ValueError("OPENWEATHERMAP_API_KEY is not set")
     else:
         logging.info("OPENWEATHERMAP_API_KEY is set")
+        weather_command = commands.Command(weather, name='wetter')
+        bot.add_command(weather_command)
 except ValueError as e:
     logging.error(e)  # Log the error message
     exit(1)  # Exit the program with a status code of 1
