bd75c3c07f9c9296ba1160605822b652265e2895
Erik Boinski <erik.boinski@posteo.de>
2024-04-09 00:46:52 +0200

    :ambulance: Critical hotfix.

--- bot_functions/weather.py
+++ bot_functions/weather.py
@@ -11,39 +11,39 @@ ToDos:
 import requests
 import logging
 from discord.ext import commands
-async def weather(ctx):
-    """
-    This function sends the current weather in a given city when the 'wetter' command is used.
-    """
-    await ctx.message.delete
-    logging.info(f"Received weather request from {ctx.author.name}")
-    lat = "52.4429081"
-    lon = "13.4424778"
-    # Coordinates for Annedore-Leber-Berufsbildungswerk/Berlin-Gropiusstadt
-    logging.info(f"Requesting weather data for {lat}, {lon}")
-    base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    complete_url = f"{base_url}&lat={lat}&lon={lon}&appid={OPENWEATHERMAP_API_KEY}&units=metric&lang=de"
-
-    logging.info(f"Received response from OpenWeatherMap API")
-    response = requests.get(complete_url)
-    weather_data = response.json()
-
-    logging.debug(f"Response: {weather_data}")
-    logging.info(f"Sending weather data to {ctx.author.name}")
-
-    if weather_data["cod"] != "404":
-        main = weather_data["main"]
-        weather = weather_data["weather"]
-        temperature = main["temp"]
-        feels_like = main["feels_like"]
-        description = weather[0]["description"]
-
-        await ctx.send(f"Die aktuelle Temperatur beträgt {temperature}°C. Es fühlt sich an wie {feels_like}°C. "
-                       f"Die Wetterbeschreibung lautet: {description}.")
-    else:
-        await ctx.send("Etwas ist schiefgelaufen.")
-
 
 def weather_setup(bot):
-    bot.add_command(weather)
-    logging.info("Weather command added to bot")
+    @commands.command(name="wetter")
+    async def weather_api(ctx):
+        """
+        This function sends the current weather in a given city when the 'wetter' command is used.
+        """
+        await ctx.message.delete
+        logging.info(f"Received weather request from {ctx.author.name}")
+        lat = "52.4429081"
+        lon = "13.4424778"
+        # Coordinates for Annedore-Leber-Berufsbildungswerk/Berlin-Gropiusstadt
+        logging.info(f"Requesting weather data for {lat}, {lon}")
+        base_url = "http://api.openweathermap.org/data/2.5/weather?"
+        complete_url = f"{base_url}&lat={lat}&lon={lon}&appid={OPENWEATHERMAP_API_KEY}&units=metric&lang=de"
+
+        logging.info(f"Received response from OpenWeatherMap API")
+        response = requests.get(complete_url)
+        weather_data = response.json()
+
+        logging.debug(f"Response: {weather_data}")
+        logging.info(f"Sending weather data to {ctx.author.name}")
+
+        if weather_data["cod"] != "404":
+            main = weather_data["main"]
+            weather = weather_data["weather"]
+            temperature = main["temp"]
+            feels_like = main["feels_like"]
+            description = weather[0]["description"]
+
+            await ctx.send(f"Die aktuelle Temperatur beträgt {temperature}°C. Es fühlt sich an wie {feels_like}°C. "
+                           f"Die Wetterbeschreibung lautet: {description}.")
+        else:
+            await ctx.send("Etwas ist schiefgelaufen.")
+
+    bot.add_command(weather_api)
